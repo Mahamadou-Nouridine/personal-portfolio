be5b794c0b1cce4a1dfe23783a0993cf4f4ae560
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-10-20 14:32:37 +0100

    Make testimonial modal closing easier

--- src/app/components/testimonial/TestimonialModal.jsx
+++ src/app/components/testimonial/TestimonialModal.jsx
@@ -12,14 +12,6 @@ const TestimonialModal = ({ show, handleClose, selected }) => {
       <div className={`overlay ${show ? "active" : ""}`} data-overlay></div>
 
       <section className="testimonials-modal">
-        <button
-          onClick={handleClose}
-          className="modal-close-btn"
-          data-modal-close-btn
-        >
-          <ion-icon name="close-outline"></ion-icon>
-        </button>
-
         <div className="modal-img-wrapper">
           <figure className="modal-avatar-box">
             <Image
@@ -40,7 +32,11 @@ const TestimonialModal = ({ show, handleClose, selected }) => {
 
           <time dateTime="2021-06-14">{selected.date}</time>
           <div className="info-content">
-            <Button href={selected.link} target="_blank" className="title m-0 mb-2 bg-primary">
+            <Button
+              href={selected.link}
+              target="_blank"
+              className="title m-0 mb-2 bg-primary"
+            >
               {" "}
               View on{" "}
               {selected.source[0].toUpperCase() + selected.source.slice(1)}
@@ -51,6 +47,21 @@ const TestimonialModal = ({ show, handleClose, selected }) => {
           <div data-modal-text>
             <p>{selected.text}</p>
           </div>
+          <Button
+            onClick={handleClose}
+            style={{
+              width: 70,
+              right: 10,
+              bottom: 5,
+              backgroundColor: "grey",
+              alignSelf: "end",
+            }}
+            className="text-white"
+            variant=""
+            data-modal-close-btn
+          >
+            Close
+          </Button>
         </div>
       </section>
     </div>
